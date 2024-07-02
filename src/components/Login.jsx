3e13d088036a7bf5aa98b3007f313cbc422b87a9
MelShared <mstinoco@espe.edu.ec>
2024-07-01 21:49:44 -0500

    inputs login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { useUser } from '../UserContext';
+import { FormControl, InputLabel, Input, InputAdornment } from '@mui/material';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 function Login() {
   const [username, setUsername] = useState('');
@@ -11,21 +14,51 @@ function Login() {
   };
 
   return (
+
     <div>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={onLogin}>Login</button>
-      {errors.length > 0 && <div>{errors.join(', ')}</div>}
+
+      <div className='gridCentrao'>
+        <form onSubmit={handleLogin}>
+          <FormControl variant="standard">
+            <InputLabel htmlFor="input-with-icon-adornment">
+              Usuario
+            </InputLabel>
+            <Input
+              type="text"
+              id="input-with-icon-adornment"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              startAdornment={
+                <InputAdornment position="start">
+                  <PersonOutlineOutlinedIcon />
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <br></br>
+          <FormControl variant="standard">
+            <InputLabel htmlFor="input-with-icon-adornment">
+              Contraseña
+            </InputLabel>
+            <Input
+              type="password"
+              id="input-with-icon-adornment"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              startAdornment={
+                <InputAdornment position="start">
+                  <LockOutlinedIcon />
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <br></br>
+          <button onClick={onLogin}>Login</button>
+          {errors.length > 0 && <div>{errors.join(', ')}</div>}
+        </form>
+      </div>
+
+
     </div>
   );
 }
