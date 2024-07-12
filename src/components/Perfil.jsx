1ae07bcee5fd86d90b3114df04a6e92cf3f28032
Chaps0218 <140579994+Chaps0218@users.noreply.github.com>
2024-07-11 19:03:25 -0500

    Estilos Perfil
    Unos cuantos estilos para el perfil, falta el popup

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useUser } from '../UserContext';
 import '../App.css';
+import './popups/popup.css';
+import Button from '@mui/material/Button';
 import PopupCambioPassword from './popups/PopupCambioPassword';
 
 function Perfil() {
@@ -16,51 +18,51 @@ function Perfil() {
     }
 
     return (
-        <div className='gridCentrao'>
+        <div className='gridCentrao grid-2row-top perfilFontSize'>
             <h1>Perfil</h1>
-            <div className='gridCentrao grid-3row-equal'>
-                <div>
-                    <div className='cuadrado-gris'>
+            <div className='gridCentrao grid-3row-equal perfilFontSize'>
+                <div className='gridCentrao grid-2row-top perfilFontSize'>
+                    <div className='gridCentrao5 cuadrado-gris'>
                         Información Personal
                     </div>
-                    <div className='gridCentrao grid-2row-equal'>
-                        <div>
+                    <div className='gridCentraoNoFull grid-2row-equal-lessSpace perfilFontSize'>
+                        <div className='gridCentraoNoFull grid-2colum-equal-lessSpace perfilFontSize'>
                             <strong>Nombre:</strong>
                             <p>{user.usu_nombre}</p>
                         </div>
-                        <div>
+                        <div className='gridCentraoNoFull grid-2colum-equal-lessSpace perfilFontSize'>
                             <strong>Apellido:</strong>
                             <p>{user.usu_apellido}</p>
                         </div>
                     </div>
                 </div>
-                <div>
-                    <div className='cuadrado-gris'>
+                <div className='gridCentrao grid-2row-top perfilFontSize'>
+                    <div className='gridCentrao5 cuadrado-gris'>
                         Permisos
                     </div>
-                    <div className='gridCentrao grid-2row-equal'>
-                        <div>
+                    <div className='gridCentraoNoFull grid-2row-equal-lessSpace perfilFontSize'>
+                        <div className='gridCentraoNoFull grid-2colum-equal-lessSpace perfilFontSize'>
                             <strong>Rol:</strong>
                             <p>{user.usu_rol}</p>
                         </div>
-                        <div>
+                        <div className='gridCentraoNoFull grid-2colum-equal-lessSpace perfilFontSize'>
                             <strong>Establecimiento:</strong>
                             <p>{user.est_nombre}</p>
                         </div>
                     </div>
                 </div>
-                <div>
-                    <div className='cuadrado-gris'>
+                <div className='gridCentrao grid-2row-top perfilFontSize'>
+                    <div className='gridCentrao5 cuadrado-gris'>
                         Seguridad
                     </div>
-                    <div className='gridCentrao grid-2row-equal'>
-                        <div>
+                    <div className='gridCentraoNoFull grid-2row-equal-lessSpace perfilFontSize'>
+                        <div className='gridCentraoNoFull grid-2colum-equal-lessSpace perfilFontSize'>
                             <strong>Usuario:</strong>
                             <p>{user.usu_user}</p>
                         </div>
-                        <div>
+                        <div className='gridCentraoNoFull grid-2colum-equal-lessSpace perfilFontSize'>
                             <strong>Contraseña:</strong>
-                            <button onClick={() => handleOpenPopup()}>Cambiar contraseña</button>
+                            <Button variant="contained" onClick={handleOpenPopup}>Cambiar Contraseña</Button>
                         </div>
                     </div>
                 </div>
